a3a07210e085fae68cf71f74a8626405c57a89f2
Clems <clems.tixier@gmail.com>
2013-02-12 22:35:00 +0100

    Modification Base.java En gros, création de tout le code

--- PROPPS/src/com/polytech/bdd/Base.java
+++ PROPPS/src/com/polytech/bdd/Base.java
@@ -1,8 +1,12 @@
 package com.polytech.bdd;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 
@@ -12,6 +16,8 @@ public class Base {
 	private static String password;
 	
 	private Connection connection;
+	private CallableStatement call;
+	
 	
 	public static void initBase(String url,String usr, String pass) throws ClassNotFoundException {
 		urlMySQL = url;
@@ -24,8 +30,55 @@ public class Base {
 		connection = DriverManager.getConnection( urlMySQL, user,password); 
 	}
 	
+	public void procedureInit(String name,int nbArg) throws SQLException {
+		String sql = "{call " + name + "(";
+		for(int i = 0 ; i < nbArg - 1 ; i++) {
+			sql += "?,";
+		}
+		sql += "?)}";
+		call = connection.prepareCall(sql);
+	}
+	
+	public void setParamString(String key, String value) throws SQLException {
+		call.setString(key, value);
+	}
+	
+	public void setParamInt(String key, int value) throws SQLException {
+		call.setInt(key, value);
+	}
+	
+	public void setParamBool(String key, boolean value) throws SQLException {
+		call.setBoolean(key, value);
+	}
+	
+	public void setParamDate(String key, Date date) throws SQLException {
+		call.setDate(key, date);
+	}
+	
+	public ResultSet executeQuery() throws SQLException {
+		return call.executeQuery();
+	}
+	
+	public boolean execute() throws SQLException {
+		return call.execute();
+	}
+	
+	public ResultSet executeSQLWithQuery(String sql) throws SQLException {
+		Statement st = connection.createStatement();
+		st.execute(sql);
+		ResultSet result = st.getResultSet();
+		st.close();
+		return result;
+	}
+	
+	public void executeSQL(String sql) throws SQLException {
+		Statement st = connection.createStatement();
+		st.execute(sql);
+		st.close();
+	}
+	
 	public void close() throws SQLException {
-		if(connection == null) {
+		if(connection != null) {
 			connection.close();
 		}
 	}
