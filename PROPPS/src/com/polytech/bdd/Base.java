17f14266b393988a13febfcc2003c189fe60d466
Clems <clems.tixier@gmail.com>
2013-02-12 22:48:17 +0100

    Ajout des commentaires Base.java

--- PROPPS/src/com/polytech/bdd/Base.java
+++ PROPPS/src/com/polytech/bdd/Base.java
@@ -26,10 +26,22 @@ public class Base {
 		Class.forName( "com.mysql.jdbc.Driver" );
 	}
 	
+	/**
+	 * Méthode de connection
+	 * 
+	 * @throws SQLException
+	 */
 	public void connect() throws SQLException {
 		connection = DriverManager.getConnection( urlMySQL, user,password); 
 	}
 	
+	/**
+	 * Méthode d'initialisation de l'appel à une procédure stockée.
+	 * 
+	 * @param name le nom de la procédure
+	 * @param nbArg le nombre d'arguments de la procédure
+	 * @throws SQLException
+	 */
 	public void procedureInit(String name,int nbArg) throws SQLException {
 		String sql = "{call " + name + "(";
 		for(int i = 0 ; i < nbArg - 1 ; i++) {
@@ -40,7 +52,8 @@ public class Base {
 	}
 	
 	/**
-	 * Methode permettant
+	 * Methode permettant de modifier un paramètre de type String pour la 
+	 * procédure initialisée
 	 * 
 	 * @param key le nom du paramètre
 	 * @param value la valeur du paramètre
@@ -50,26 +63,70 @@ public class Base {
 		call.setString(key, value);
 	}
 	
+	/**
+	 * Methode permettant de modifier un paramètre de type int pour la 
+	 * procédure initialisée
+	 * 
+	 * @param key le nom du paramètre
+	 * @param value la valeur du paramètre
+	 * @throws SQLException
+	 */
 	public void setParamInt(String key, int value) throws SQLException {
 		call.setInt(key, value);
 	}
 	
+	/**
+	 * Methode permettant de modifier un paramètre de type Boolean pour la 
+	 * procédure initialisée
+	 * 
+	 * @param key le nom du paramètre
+	 * @param value la valeur du paramètre
+	 * @throws SQLException
+	 */
 	public void setParamBool(String key, boolean value) throws SQLException {
 		call.setBoolean(key, value);
 	}
 	
+	/**
+	 * Methode permettant de modifier un paramètre de type Date pour la 
+	 * procédure initialisée
+	 * 
+	 * @param key le nom du paramètre
+	 * @param value la valeur du paramètre
+	 * @throws SQLException
+	 */
 	public void setParamDate(String key, Date date) throws SQLException {
 		call.setDate(key, date);
 	}
 	
+	/**
+	 * Methode permettand d'exécuter une requête SQL et de récupérer son
+	 * résultat sous la forme d'un resultSet
+	 * 
+	 * @return le résultat de la procédure appelée
+	 * @throws SQLException
+	 */
 	public ResultSet executeQuery() throws SQLException {
 		return call.executeQuery();
 	}
 	
+	/**
+	 * Methode permettant d'exécuter une requête SQL sans récupérer
+	 * son résultat
+	 * 
+	 * @throws SQLException
+	 */
 	public boolean execute() throws SQLException {
 		return call.execute();
 	}
 	
+	/**
+	 * Methode permettant d'exécuter une requête SQL en récupérant son résutat;
+	 * 
+	 * @param sql la requête à exécuter
+	 * @return le résultat de la requête
+	 * @throws SQLException
+	 */
 	public ResultSet executeSQLQuery(String sql) throws SQLException {
 		Statement st = connection.createStatement();
 		st.execute(sql);
@@ -78,15 +135,28 @@ public class Base {
 		return result;
 	}
 	
+	/**
+	 * Methode permettant d'exécuter une requête SQL sans récupérer
+	 * son résultat
+	 * 
+	 * @param sql la requête à exécuter
+	 * @throws SQLException
+	 */
 	public void executeSQL(String sql) throws SQLException {
 		Statement st = connection.createStatement();
 		st.execute(sql);
 		st.close();
 	}
 	
+	/**
+	 * Methode utilisée pour fermer la connection
+	 * 
+	 * @throws SQLException
+	 */
 	public void close() throws SQLException {
 		if(connection != null) {
 			connection.close();
 		}
+		call = null;
 	}
 }
