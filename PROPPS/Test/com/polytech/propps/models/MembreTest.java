77097a8d778b11ac24eaf6c3a345c4511c6003b8
Clems <clems.tixier@gmail.com>
2013-02-16 23:34:08 +0100

    Ajout dossier Test et configuration JUnit

---
+++ PROPPS/Test/com/polytech/propps/models/MembreTest.java
@@ -0,0 +1,31 @@
+package com.polytech.propps.models;
+
+import org.junit.*;
+
+public class MembreTest {
+	
+	@BeforeClass
+	public static void setUpClass() {
+		
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		
+	}
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void insert() {
+		
+	}
+}
