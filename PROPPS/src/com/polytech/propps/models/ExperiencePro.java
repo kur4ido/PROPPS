2276631a268068ef17dec9f8c906bfd53184825e
Clems <clems.tixier@gmail.com>
2013-02-16 16:02:42 +0100

    Ajout Model ExperiencePro

---
+++ PROPPS/src/com/polytech/propps/models/ExperiencePro.java
@@ -0,0 +1,22 @@
+package com.polytech.propps.models;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class ExperiencePro {
+	protected String sPosteOccupe;
+	protected String sDescription;
+	protected String sDirection;
+	protected Date dtDebut;
+	protected Date dtFin;
+	protected ArrayList<Integer> lstProfils;
+	protected int ID_Profil;
+	protected int ID_ExpPro;
+	
+	private boolean bFill;
+	
+	public ExperiencePro(int ID) {
+		bFill = false;
+		this.ID_ExpPro = ID;
+	}
+}
