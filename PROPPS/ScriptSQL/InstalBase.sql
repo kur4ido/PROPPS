13cf1a7c09df5c79022670bb0e01ac4bd9163f36
Clems <clems.tixier@gmail.com>
2013-02-20 17:39:42 +0100

    Modif Install Base

--- PROPPS/ScriptSQL/InstalBase.sql
+++ PROPPS/ScriptSQL/InstalBase.sql
@@ -888,6 +888,7 @@ CREATE PROCEDURE `PROPPS_DB`.`Recruteur_getByID` (IN _ID_Utilisateur INT)
 BEGIN
     SELECT * 
     FROM Utilisateur inner join Recruteur on Utilisateur.ID_Utilisateur = Recruteur.ID_Utilisateur
+        inner join Adresse on Adresse.ID_Adresse = Utilisateur.ID_Adresse
     WHERE Recruteur.ID_Utilisateur = _ID_Utilisateur;
 END$$
 
