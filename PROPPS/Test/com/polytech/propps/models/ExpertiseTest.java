4dd7e2f839668774eedc4516c388d527a5f2f0f9
alexandre <alexandre@192.168.0.42>
2013-02-22 08:14:22 +0100

    Ajout de ExpertiseTest + MAJ UnitTestCenter

---
+++ PROPPS/Test/com/polytech/propps/models/ExpertiseTest.java
@@ -0,0 +1,52 @@
+package com.polytech.propps.models;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.polytech.propps.appli.PROPPS;
+
+public class ExpertiseTest {
+
+	
+	@BeforeClass
+	public static void setUpClass() throws IOException, ClassNotFoundException {
+		
+		PROPPS.init();
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		
+	}
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		// TO-DO delete xpPro1s	
+	}
+	
+	@Test
+	public void equalsTest() {
+		
+		Expertise e1 = new Expertise(1);
+		Expertise e1_bis = new Expertise(1);
+		Expertise e2 = new Expertise(100);
+		
+		
+		Assert.assertTrue("e1 et e1_bis devraient etre egaux", e1.equals(e1_bis));
+		Assert.assertFalse("e1 et e2 devraient etre egaux", e1.equals(e2));
+		
+	}
+}
--- PROPPS/Test/com/polytech/propps/testingCenter/UnitTestCenter.java
+++ PROPPS/Test/com/polytech/propps/testingCenter/UnitTestCenter.java
@@ -4,10 +4,18 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.polytech.propps.models.AdresseTest;
 import com.polytech.propps.models.MembreTest;
+import com.polytech.propps.models.MessageTest;
+import com.polytech.propps.models.Notification;
+import com.polytech.propps.models.NotificationTest;
+import com.polytech.propps.models.ProfilTest;
+import com.polytech.propps.models.RecruteurTest;
+import com.polytech.propps.models.SocieteTest;
+import com.polytech.propps.models.UtilisateurTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ MembreTest.class})
+@SuiteClasses({ MembreTest.class, RecruteurTest.class, AdresseTest.class, MessageTest.class, NotificationTest.class, ProfilTest.class, SocieteTest.class, UtilisateurTest.class})
 public class UnitTestCenter {
 
 	
